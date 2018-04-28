fb58049d4d98b241b9b20f1fa86bd0aa49a874cc
sanuv9683 <sanuv9683@gmail.com>
2018-04-28 11:36:50 +0530

    done

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './view/home/home.component';
+import { AdminHomeComponent } from './view/admin-home/admin-home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AdminHomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    MDBBootstrapModule.forRoot()
+  ],
+  schemas: [ NO_ERRORS_SCHEMA ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
